5a117ab2c5a3380751e6ea10d270d0acf13821f8
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-11-19 08:53:12 +0100

    1847.03  añadimos un nuevo atributo y un metodo correspondiente para que nos cuente las paginas del libro

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int numeroPaginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroPaginasLibro)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numeroPaginasLibro;
     }
     public String getNombreAutor(){
         return autor;
@@ -33,7 +35,9 @@ public class Libro {
     public void imprimeTitulo() {
         System.out.println(titulo);
     }
-
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
 
 
 
