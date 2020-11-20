b9292b965b0a784f251279bff4effaa118307cc2
Pablo1038 <pablogutierrezramos1038rgmail.com>
2020-11-20 08:49:44 +0100

    1847.05  Añadimos un atributo adicional llamado numeroReferncia, añadimos dos nuevos metodos getter y setter y modificamos los atributos imprimirDetalles y getDetalles

--- Libro.java
+++ Libro.java
@@ -13,6 +13,7 @@ public class Libro {
     private String autor;
     private String titulo;
     private int numeroPaginas;
+    private String numeroReferencia;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
@@ -22,6 +23,7 @@ public class Libro {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas = numeroPaginasLibro;
+        numeroReferencia = "";
     }
     public String getNombreAutor(){
         return autor;
@@ -38,15 +40,29 @@ public class Libro {
     public int getNumeroPaginas() {
         return numeroPaginas;
     }
-    public void imprimirDetalles() {
-        System.out.println("Titulo: " + titulo + "," + " Autor: " + autor + "," + " Páginas: " + numeroPaginas);
+    public void imprimirDetalles() {    
+        System.out.println("Titulo: " + titulo + "," + " Autor: " + autor + "," + " Páginas: " + numeroPaginas + "Número de Referencia: " + numeroReferencia);
         
     }
     public String getDetalles() {
-        String getDetalles = "Titulo: " + titulo + "," + " Autor: " + autor + "," + " Páginas: " + numeroPaginas; 
-        return getDetalles;
+        String detalles = "Titulo: " + titulo + "," + " Autor: " + autor + "," + " Páginas: " + numeroPaginas + "Número de Referencia: " + numeroReferencia; 
+        if ("" != (numeroReferencia)) {
+            System.out.println("Numero de Referencia: " + numeroReferencia);
+        }
+        else{
+            System.out.println("Número de referencia: ZZZ" );
+        }    
+        return detalles;
     }
-
-
+    public void setNumeroReferencia(String nuevoNumeroReferencia) {
+        int numeroCaracteresReferencia = 3;
+        if(numeroReferencia.length() < 3){
+           System.out.println("Se necesitan 3 o más caracteres para realizar este metodo.");
+        }
+        numeroReferencia = nuevoNumeroReferencia;
+    }
+    public String getNumeroReferencia() {
+        return numeroReferencia;
+    }
 }
 
